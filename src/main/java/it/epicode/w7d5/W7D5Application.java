6dc4715460d475373dbc47fb8a830158ae057dbf
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-02-23 08:55:22 +0100

    commit inizilizer project

---
+++ src/main/java/it/epicode/w7d5/W7D5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.w7d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W7D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W7D5Application.class, args);
+	}
+
+}
